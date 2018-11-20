7b8e3e43c0d9c5c32c7075b8e8e53f6c8e34aff3
jbbraley <jbbraley@gmail.com>
2018-11-20 18:05:14 -0500

    backup FE simulation data

---
+++ Code/profile/evaluation/batch_process_profiles.m
@@ -0,0 +1,28 @@
+%% select folder containing profiles
+
+%% set bridge and vehicle parameters
+% vehicle
+
+% get bridge parameters from stiffness value and 1st nat. freq.
+
+
+%% get all files in folder
+
+
+%% setup state space model
+% initialize
+
+% assign parameters
+
+%% Loop through profiles
+% preallocate for results
+
+for ii = 1:length(fnames)
+   % load profile
+   
+   % assign to model
+   
+   % run
+   
+   % compute amplification
+end
